4785b55692355ce960c50c3b894ce9aec816099c
[spencer lynch] <[spencer.lynch9@yahoo.com]>
2017-09-27 20:37:01 -0400

    updated all of the index.html photos with new links to local folder

--- js/filter.js
+++ js/filter.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 
-=======
->>>>>>> 8e95227234e042be9d3e499c7563d0fa038d7d0f
 //Function for data filter on paintings.html
 $(document).ready(function(){
 
@@ -29,11 +26,8 @@ $(this).removeClass("active");
 $(this).addClass("active");
 
 });
-<<<<<<< HEAD
 
 
 
 
 
-=======
->>>>>>> 8e95227234e042be9d3e499c7563d0fa038d7d0f
